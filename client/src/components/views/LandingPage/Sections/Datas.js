5442cf51fca2c31e683e2ccadf2533863bbc65fc
Suraj Ghodke <surajghodke0102@gmail.com>
2020-04-07 23:41:47 +0530

    heroku deployment

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,118 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "₹0 to ₹99",
+        "array": [0, 99]
+    },
+    {
+        "_id": 2,
+        "name": "₹100 to ₹149",
+        "array": [100, 149]
+    },
+    {
+        "_id": 3,
+        "name": "₹150 to ₹249",
+        "array": [150, 249]
+    },
+    {
+        "_id": 4,
+        "name": "₹250 to ₹399",
+        "array": [250, 399]
+    },
+    {
+        "_id": 5,
+        "name": "More than ₹400",
+        "array": [400, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Veg"
+    },
+    {
+        "_id": 2,
+        "name": "Non-Veg"
+    },
+    {
+        "_id": 3,
+        "name": "Chinese"
+    },
+    {
+        "_id": 4,
+        "name": "Breakfast"
+    },
+    {
+        "_id": 5,
+        "name": "South Indian"
+    },
+    {
+        "_id": 6,
+        "name": "North Indian"
+    },
+    {
+        "_id": 7,
+        "name": "Sea Food"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
+
+/*
+const Continents = [
+    { key: 1, value: "Veg" },
+    { key: 2, value: "Non-Veg" },
+    { key: 3, value: "Chinese" },
+    { key: 4, value: "Breakfast" },
+    { key: 5, value: "South Indian" },
+    { key: 6, value: "North Indian" },
+    { key: 7, value: "Sea Food" }
+]
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$200 to $249",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "$250 to $279",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "$280 to $299",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $300",
+        "array": [300, 1500000]
+    }
+]
+
+*/
